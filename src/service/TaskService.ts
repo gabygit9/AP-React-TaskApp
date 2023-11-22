c7e17bada7ca216694da8102354d9da7cd169b4f
Gabriela Camacho <gabrielacamacho09@hotmail.com>
2023-11-22 08:09:27 -0300

    renderTask

--- src/service/TaskService.ts
+++ src/service/TaskService.ts
@@ -1,6 +1,6 @@
 import { Task } from "../types/Task";
 
-const BASE_URL = "http://localhost:3000/tasks";
+const BASE_URL = "https://ap-back-taskapp.onrender.com/tasks";
 
 export const TaskService = {
     getAllTasks: async (): Promise<Task[]> => {
